7d5abae51b5a15332f486f6cfee2d1a12f8f1017
Stéphane Boulanger <Stphane Boulanger@LAPTOP-R5MCKF6L>
2021-07-20 10:22:32 -0400

    Ajoutez des fichiers projet.

---
+++ AlgoTri/main.cpp
@@ -0,0 +1,26 @@
+/**
+ * \file main.cpp
+ * \brief 
+ * \author Stéphane Boulanger 
+ * \version 0.1
+ * \date juillet 2021
+ */
+
+# include <iostream>
+
+int main()
+{
+
+
+
+
+
+
+
+
+	std::cout << "Hello world" << std::endl; 
+
+
+
+	return 0; 
+}
